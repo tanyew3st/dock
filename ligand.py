141c5de1773b39e6bd0b2134d5b147585782c20d
Tanay <chandak.tanay@gmail.com>
2020-05-18 17:22:29 -0500

    f

--- ligand.py
+++ ligand.py
@@ -1,3 +1,3 @@
-
+# class variables for each ligand that need to be changed
 def __init__(self):
     print("something")
--- reader.py
+++ reader.py
@@ -1,1 +1,7 @@
+# This class will drive the reading of files and changing input to .pdbqt
+
+import ligand
+
+# This method will read the .txt file for our local use
+# and return a Object of type Ligand
 readFile()
