96be2befd589bfe898b540380ad5f7a6fea5b95d
Armaan <armaan.chandak@gmail.com>
2020-05-18 11:38:07 -0500

    add another change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 import os;
 import subprocess;
-print("hello wolrd and armaan and tanay")
+print("hello wolrd and armaan and tana another cfhangey")
 
 command = "./vina";
 os.system(command)
