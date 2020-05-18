d63423a239003d8980b8fc26909e90521923f081
Tanay <chandak.tanay@gmail.com>
2020-05-18 17:26:54 -0500

    f

--- reader.py
+++ reader.py
@@ -2,6 +2,11 @@
 
 import ligand
 
-# This method will read the .txt file for our local use
-# and return a Object of type Ligand
-readFile()
+# This method will read the .txt file for our local use and return a file
+# Return a file object
+def openFile():
+    # method body
+
+# Take in a file object and return a new Ligand object
+def openFile():
+    # method body
