292b7f755eb01e3d4fe7e691b4afe7aa4bb96133
Armaan <armaan.chandak@gmail.com>
2020-05-18 17:27:29 -0500

    asdf

--- ligand.py
+++ ligand.py
@@ -1,3 +1,18 @@
 # class variables for each ligand that need to be changed
+receptor = None
+ligand = None
+
+center_x = None
+center_y = None
+center_z = None
+
+size_x = None
+size_y = None
+size_z = None
+
+exhaustiveness = None
+
+
+
 def __init__(self):
     print("something")
