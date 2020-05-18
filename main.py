d36ebdd9c6bcc923b807edc7624e0d03b66a6a4e
Tanay <chandak.tanay@gmail.com>
2020-05-18 18:07:05 -0500

    push1

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-import os
 import ligand
 import os;
 
-protein = "EGFR";
+# selected by the user upon initial interaction with the API
+protein = "EGFR"
 
-def main():
+directory = None
+
+proteins = []
+
+def dock(protein):
+    command = ""
+
+if __name__ == "__main__":
     # command = "./vina"
     # os.system(command)
 
-    property = ligand.openFile("conf.txt")
-    ligand.readFile(property)
-
-    print ligand.properties
+    # property = ligand.openFile("conf.txt")
+    # ligand.readFile(property)
+    #
+    # print ligand.properties
 
     # choose name
     #change 2
 
+    for i in os.listdir("proteins"):
+        print(i)
+        directory = "proteins/" + i
+        print(directory)
 
+    for i in os.listdir(directory):
+        print("Protein" + i)
 
-main()
+        # directory for the conf.txt
+        prtdir = directory + "/" + i + "/conf.txt"
+        print(prtdir)
+        # proteins.append(ligand(prtdir))
+
+    for i in protein:
+        dock(i)
