4f07437cf1b6832dcb89bcd95854a3f8e90c1fd6
Tanay <chandak.tanay@gmail.com>
2020-05-17 22:15:24 -0500

    working terminal

--- hello.py
+++
@@ -1,3 +0,0 @@
-import sys, string, os
-
-os.system("C://Users/tchandak/instraFinal/dock/vina")
---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+import os;
+import subprocess;
+print("hello wolrd")
+
+command = "/Users/tchandak/Desktop/Dock/dock/vina";
+os.system(command)
