95233a2883d793442fb79d3ba142662007f31db3
Tanay <chandak.tanay@gmail.com>
2020-05-18 19:16:02 -0500

    make proetins

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import ligand
-import os;
+from __future__ import print_function
+import os
+from protein import Protein
 
 # selected by the user upon initial interaction with the API
 protein = "EGFR"
@@ -15,30 +16,17 @@ if __name__ == "__main__":
     # command = "./vina"
     # os.system(command)
 
-    # property = ligand.openFile("conf.txt")
-    # ligand.readFile(property)
-    #
-    # print ligand.properties
-    property = ligand.openFile("conf.txt")
-    ligand.readFile(property)
-
-    print(ligand.properties)
-
-    # choose name
-    #change 2
-
     for i in os.listdir("proteins"):
-        print(i)
         directory = "proteins/" + i
-        print(directory)
 
     for i in os.listdir(directory):
-        print("Protein" + i)
 
         # directory for the conf.txt
         prtdir = directory + "/" + i + "/conf.txt"
-        print(prtdir)
-        # proteins.append(ligand(prtdir))
 
-    for i in protein:
-        dock(i)
+        # if to get rid of .ds-store and other weird files
+        if len(i) <= 4:
+            p1 = Protein(prtdir)
+            proteins.append(p1)
+
+    print(*proteins,sep='\n')
---
+++ protein.py
@@ -0,0 +1,37 @@
+# class variables for each Protein that need to be changed
+class Protein:
+    properties = {
+        "receptor": None,
+        "center_x": None,
+        "center_y": None,
+        "center_z": None,
+        "size_x": None,
+        "size_y": None,
+        "size_z": None,
+        "exhaustiveness": None
+    }
+
+    def __repr__(self):
+        return str(self.properties)
+        # return str(self.x)
+
+    def openFile(self, name):
+        propertyUO = {}
+        o = open(name)
+        opened = o.readlines()
+        for i in opened:
+            if i != "\n":
+                propertyUO[i.split()[0]] = i.split()[-1]
+        return propertyUO
+
+    def __init__(self, name):
+        self.x = name
+        propertyUO = self.openFile(name)
+        self.properties = propertyUO
+        # self.readFile(propertyUO)
+
+
+    # Take in a file object and return a new Protein object
+    def readFile(self, propertyUO):
+        for i in propertyUO:
+            self.properties[i] = propertyUO[i]
