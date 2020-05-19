38758abae2b065fbaa61b29d050933aca28011f1
Tanay <chandak.tanay@gmail.com>
2020-05-19 15:03:19 -0500

    added files

--- ml.py
+++ ml.py
@@ -4,8 +4,10 @@ class MachineLearning:
     def __init__(self, affinity):
         self.affinity = affinity
 
-    # input is the location of the pdbqt files
+    # input is the location of the folder of the pdbqt files
     # output is where it will be printed to
-    def createDirectory(inputdir, outputdir):
+    # boolean active to add ticker 1 or 0 to the excel spreadsheet
+    def createDirectory(inputdir, outputdir, active):
         print('something')
 
+        # end product to create a new excel spreadsheet .xlsx in the output location
